631027d82f8a8a2b4a9ddf59ec68414bf425b4a7
CyraxVip <128605688+CyraxVip@users.noreply.github.com>
2023-11-22 21:11:08 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,10 @@
-export USE_CCACHE=1
-export CCACHE_EXEC=/usr/bin/ccache
-ccache -M 15G
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo twrp_a30s-user
+add_lunch_combo twrp_a30s-userdebug
+add_lunch_combo twrp_a30s-eng
